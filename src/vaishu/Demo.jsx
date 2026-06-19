3e7c82672d2bba62afed92e6ec219d645e7488a0
mittavaishnavi <mittavaishnavi718@gmail.com>
2026-06-19 20:05:40 +0530

    added color message

--- src/vaishu/Demo.jsx
+++ src/vaishu/Demo.jsx
@@ -1,11 +1,22 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
 import "./Demo.css";
 function Demo() {
     const [color,setColor]=useState("white");
+    const [message,setMessage]=useState("");
+
+    useEffect(()=>
+    {   
+        setMessage(`color changed to ${color}`);
+        alert(`color changed to ${color}`);
+    },[color]);
     return ( 
         <div className="box"
             style={{background: color}}>
                 <h1>color changer</h1>
+                <h2>
+                    {message}
+                </h2>
                 <button onClick={()=>setColor("pink")}>pink</button>
                 <button onClick={()=>setColor("blue")}>blue</button>
                 <button onClick={()=>setColor("green")}>green</button>
